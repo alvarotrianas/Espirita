b0f0ea0de293ad32f98cd7566e3b89c95e4f8cec
Davsatu313 <davsatu313@hotmail.com>
2020-04-04 11:55:06 -0500

    Enemy chase animations

---
+++ Source/Espirita/EspiritaInterfaces/PatrollObject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PatrollObject.h"
+
+// Add default functionality here for any IPatrollObject functions that are not pure virtual.
